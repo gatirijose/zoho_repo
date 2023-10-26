2c23e235ccd4cb8d6e7af49ff0899477edf684cd
Joseph waweru <wawerugatiri@gmail.com>
2023-10-26 17:44:47 +0000

    update

--- mailing/views.py
+++ mailing/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.core.mail import send_mail
+from django.conf import settings 
 # Create your views here.
+
+
+def index(request):
+    if request.method == 'POST':
+        subject = 'Trial and Error'
+        message  = request.POST['message']
+        receiver = ['gatirijose@gmail.com',]
+
+        send_mail(subject, message, receiver)
+    return render(request, 'index.html')
